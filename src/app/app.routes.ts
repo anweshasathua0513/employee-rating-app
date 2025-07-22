8a49f295079ee8782d049aa2b3942cee19dfd679
anweshasathua0513 <anweshasathua0513@gmail.com>
2025-07-22 15:13:27 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { EmployeeRatingComponent } from './employee-rating/employee-rating.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '', // default route when app loads
+    component: EmployeeRatingComponent
+  }  
+];
