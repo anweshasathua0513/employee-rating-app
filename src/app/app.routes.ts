690a5b33fb35d939ea731a76f3d35ee1b09e3421
anweshasathua0513 <anweshasathua0513@gmail.com>
2025-07-22 16:25:55 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { EmployeeRatingComponent } from './app/employee-rating/employee-rating.component';
+import { EmployeeRatingComponent } from './employee/employee-rating.component';
 
 export const routes: Routes = [
     {
