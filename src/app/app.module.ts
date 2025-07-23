299a51094e40f4ee343bfa576e10501f36450446
anweshasathua0513 <anweshasathua0513@gmail.com>
2025-07-23 10:14:54 +0530

    Delete src directory

--- src/app/app.module.ts
+++
@@ -1,1 +0,0 @@
-// This file is no longer needed because AppComponent is standalone and should be bootstrapped via main.ts
--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { EmployeeRatingComponent } from './employee/employee-rating.component';
-
-export const routes: Routes = [
-    {
-      path: '', // default route when app loads
-    component: EmployeeRatingComponent
-  }  
-];
